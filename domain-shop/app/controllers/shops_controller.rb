dab33933d4783008574066aabc7c0d365b9e8828
antonzaharia <zaharia_anton@yahoo.com>
2021-04-02 17:24:18 +0100

    craete and delete shop

--- domain-shop/app/controllers/shops_controller.rb
+++ domain-shop/app/controllers/shops_controller.rb
@@ -1,8 +1,18 @@
 class ShopsController < ApplicationController
+    include CurrentUserConcern
     def create
+        @shop = @current_user.shops.create(name: params[:new_shop][:name])
+        redirect_to user_path(@current_user)
     end
     
     def show
-        @show = Shop.find(params[:id])
+        @new_shop = Shop.find(params[:id])
+    end
+
+    def destroy
+        shop = Shop.find(params[:id])
+        shop.destroy
+
+        redirect_to user_path(@current_user)
     end
 end
--- domain-shop/config/routes.rb
+++ domain-shop/config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   root to: 'users#new'
   post '/', to: 'users#create'
   resources :users, :only => [:show]
-  resources :shops, :only => [:show, :create]
+  resources :shops, :only => [:show, :destroy]
+  post "/shops/create", to: "shops#create"
   delete '/logout', to: "users#destroy"
 
   get '/search', to: "statics#search"
